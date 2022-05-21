027865278f58f3aa6c1a9e6b010375cecea5719c
JoseTorquato <jose.torquato@grupoboticario.com.br>
2022-05-21 20:28:30 -0300

    feat: create success/fail in calculator view

--- src/view/calculator_view.py
+++ src/view/calculator_view.py
@@ -1,4 +1,21 @@
+import os
+
+
 class CalculatorView:
-    @staticmethod
     def get_number(self) -> str:
         return input("Digite um número: ")
+
+    def calculator_success(self) -> None:
+        self.__clear()
+
+        message = f'\tCalculo foi concluido com sucesso.'
+        print(message)
+
+    def calculator_fail(self) -> None:
+        self.__clear()
+
+        message = f'\tOcorreu um erro ao calcular.'
+        print(message)
+
+    def __clear(self):
+        os.system('cls||clear')
